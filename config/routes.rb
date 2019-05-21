e303fa38b8a3832709957f5729c790d2350f85aa
maayaizumi0326 <insta.mi0326@gmail.com>
2019-05-21 07:33:35 +0000

    userscontroller追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/confirm_new'
+  get 'users/show'
+  get 'users/edit'
+  get 'users/update'
+  get 'users/show_mypage'
+  get 'users/update_user'
+  get 'users/confirm_delete'
+  get 'users/complete_delete'
   resources :products
   resources :post_images, only: [:new, :create, :index, :show]
   devise_for :users
---
+++ test/controllers/users_controller_test.rb
@@ -0,0 +1,44 @@
+require 'test_helper'
+
+class UsersControllerTest < ActionDispatch::IntegrationTest
+  test "should get confirm_new" do
+    get users_confirm_new_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get users_show_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get users_edit_url
+    assert_response :success
+  end
+
+  test "should get update" do
+    get users_update_url
+    assert_response :success
+  end
+
+  test "should get show_mypage" do
+    get users_show_mypage_url
+    assert_response :success
+  end
+
+  test "should get update_user" do
+    get users_update_user_url
+    assert_response :success
+  end
+
+  test "should get confirm_delete" do
+    get users_confirm_delete_url
+    assert_response :success
+  end
+
+  test "should get complete_delete" do
+    get users_complete_delete_url
+    assert_response :success
+  end
+
+end
