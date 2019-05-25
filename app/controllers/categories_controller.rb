da36ec15022388359e6afe5d583a1b99d9fb861d
kazuhiro-okuda <kazuhiro.okuda.soka@gmail.com>
2019-05-25 02:19:07 +0000

    some fix

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -13,6 +13,7 @@ class CategoriesController < ApplicationController
 
   def show
   	@category = Category.find(params[:id])
+  	#@category = Products.where(category_id: params[:id])
   end
 
   def create
@@ -51,6 +52,18 @@ class CategoriesController < ApplicationController
 	end
   end
 
+  def destroy
+		category = Category.find(params[:id])
+
+		if category.destroy
+			flash[:notice] = "destroy successfully!"
+			redirect_to categories_path
+		else
+			flash[:notice] = "destroy failed"
+			redirect_to edit_category_path
+		end
+  end
+
 	private
 		def category_params
 		  	params.require(:category).permit(:category_image, :category_title)
@@ -67,3 +80,43 @@ end
 
 # 	<%= link_to 'delete', category_path(@category), method: :delete %>
 # <% end %>
+
+
+# 【new.html.rb】
+# <% if flash[:notice] %>
+# 	<p class="flash"><%= flash[:notice] %>
+# </p>
+# <% end %>
+
+# <div class="container">
+# 	<form>
+# 		<% if params[:preview] %>
+# 			<div class="row">
+# 				<p>カテゴリーページではこのように表示されます</p>
+# 				<%= @category.category_title %>
+# 				 <%= attachment_image_tag @category, :category_image, :fill, 200, 200, class: "img-rounded", fallback: "no_image.jpg",size:'100x100' %>
+# 			</div>
+# 		<% end %>
+# 		<div class="">
+# 		<%= form_for(@category) do |f| %>
+
+# 			<% if @category.errors.any? %>
+# 				<%= @category.errors.count %>
+# 				<%= @category.errors.full_messages.each do |message| %>
+# 					<%= message %>
+# 				<% end %>
+# 			<% end %>
+# 				    <div class="row">
+# 				    <label class="col-xs-3">CategoryName</label>
+# 					<%= f.text_field :category_title, autofocus: true , placeholder:'カテゴリ名を入力してください', class: "col-xs-3" %>
+# 					</div>
+# 				    <div class="row">
+# 				    <label class="col-sm-3 ">CategoryImage</label>
+# 						<%= f.attachment_field :category_image, class: "" %>
+# 					</div>
+# 						<%= f.submit 'Add New Category', class: "col-xs-3 btn btn-primary active" %>
+# 						<%= f.submit 'Preview', name: "preview", class: "col-xs-3 btn btn-default active" %>
+# 		<% end %>
+# 	</div>
+#     </form>
+# </div>
