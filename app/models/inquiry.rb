c9edcd016979fa545f6d792e72a044d85cf5230c
kazuhiro-okuda <kazuhiro.okuda.soka@gmail.com>
2019-05-26 08:33:20 +0000

    add model association

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -1,6 +1,6 @@
 class Inquiry < ApplicationRecord
 	# enum inquiry_status: { :0, 対応中:1, 未対応:2 }
-
+	belongs_to :user
 	validates :inquiry_title, presence: true
 	validates :inquiry_text, presence: true
 
