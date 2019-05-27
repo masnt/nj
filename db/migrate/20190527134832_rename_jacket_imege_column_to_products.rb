00364bbaaa99b090f6b1f30717fce202959578f0
kazuhiro-okuda <39982463+kazuhiro-okuda@users.noreply.github.com>
2019-05-28 01:22:42 +0900

    Update 20190527134832_rename_jacket_imege_column_to_products.rb

--- db/migrate/20190527134832_rename_jacket_imege_column_to_products.rb
+++ db/migrate/20190527134832_rename_jacket_imege_column_to_products.rb
@@ -1,6 +1,6 @@
 class RenameJacketImegeColumnToProducts < ActiveRecord::Migration[5.2]
   def change
-  	rename_column :produts, :jacket_image, :jacket_image_id
+  	rename_column :products, :jacket_image, :jacket_image_id
   end
 end
 
