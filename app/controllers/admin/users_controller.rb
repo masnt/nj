ddd12d506a660b5421b98b8748e0aa9734e5addc
floppycamel22 <floppycamel1022@gmail.com>
2019-05-27 12:46:41 +0000

    maigration test

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,10 +1,36 @@
 class Admin::UsersController < ApplicationController
+  def new
+  end
+
   def edit
+    @user = User.find(params[:id])
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def index
+    @users = User.all
   end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    	redirect_to admin_users_path
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to admin_users_path, notice:"#{@user.family_name}様のユーザー情報を削除しました"
+  end
+
+private
+
+  def user_params
+      params.require(:user).permit(:family_name,  :first_name, :family_name_kana, :first_name_kana, :email, :password, :password_confirmation,
+  	 :address, :phone_number, :postal_code, :admin)
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2019_05_27_072307) do
 
-
   create_table "cart_histories", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "cart_item_history_id", null: false
@@ -117,9 +116,7 @@ ActiveRecord::Schema.define(version: 2019_05_27_072307) do
     t.integer "unit_price", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "jacket_image_id"
     t.string "label"
-
   end
 
   create_table "shopinfomations", force: :cascade do |t|
