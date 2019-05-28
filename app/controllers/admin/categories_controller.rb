b47429c00c41caf84d518f6e7a687218b297ba72
kazuhiro-okuda <kazuhiro.okuda.soka@gmail.com>
2019-05-28 08:39:30 +0000

    fix categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,7 +1,19 @@
 class Admin::CategoriesController < ApplicationController
   def new
+  	@category = Category.new
   end
 
   def edit
+  	@category = Category.find(params[:id])
+  	if params[:preview]
+  		flash[:notice] = "Preview now"
+  	    render :edit
+  	elsif @category.save
+		flash[:notice] = "Add successfully!"
+		redirect_to categories_path
+	else
+		flash[:notice] = "error, Add failed"
+		render :new
+	end
   end
 end
