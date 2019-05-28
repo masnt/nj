e2490743232c2f0737b532f738256ecab9281c5c
kazutofuruta <19940718fk@gmail.com>
2019-05-28 07:13:21 +0000

    商品詳細とカートを

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,11 @@ class UsersController < ApplicationController
 
   def edit
   	 @user = User.find(params[:id])
-
-  
-  before_action :authenticate_user!
+     #before_action :authenticate_user!
+  end
 
   def cart
-    @user = current_user
+    @user = User.find(params[:user_id])
     @carts = @user.cart_items
     @total_price = @carts.sum(:sub_total)
     @total_purchase_quantity = @carts.sum(:purchase_quantity)
@@ -21,14 +20,14 @@ class UsersController < ApplicationController
   end
 
   def show
-  	 @user = User.find(params[:id])
+  	 @user = current_user
   end
 
   def update
   	 @user = User.find(params[:id])
-  if @user.update(user_params)
+     @user.update(user_params)
      redirect_to user_path(@user.id)
-  end
+
 end
 
 
@@ -53,8 +52,8 @@ end
         params.require(:user).permit(:family_name, :first_name, :family_name_kana, :first_name_kana, :postal_code, :address, :phone_number, :email)
     end
 
-  
-  def user_params
+
+  def user_item_params
     params.require(:user, :cart_item).permit(:product_name, :artist, :stock_quantity, :cart_item_id, :product_id, :purchase_quantity, :sub_total, :user_id, :unit_price, :amount)
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_28_001716) do
+ActiveRecord::Schema.define(version: 2019_05_28_064916) do
 
   create_table "cart_histories", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -119,6 +119,7 @@ ActiveRecord::Schema.define(version: 2019_05_28_001716) do
     t.datetime "updated_at", null: false
     t.string "label"
     t.string "jacket_image_id"
+    t.integer "select_stock"
   end
 
   create_table "shopinfomations", force: :cascade do |t|
