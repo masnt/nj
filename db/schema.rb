5f3b22857db7455e22b303623a0b8b9e4cfd5d49
kazuhiro-okuda <kazuhiro.okuda.soka@gmail.com>
2019-05-30 08:54:44 +0000

    fix conflict

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
-  # This file is auto-generated from the current state of the database. Instead
+# This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
@@ -102,7 +102,6 @@ ActiveRecord::Schema.define(version: 2019_05_30_054900) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "product_id"
-    t.string "product_image_id"
   end
 
   create_table "product_reviews", force: :cascade do |t|
